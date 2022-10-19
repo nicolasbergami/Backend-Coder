98f8ef36c8c09eb043fda20f23a2901e53198d8a
nicolasbergami <nicolasbergami2013@gmail.com>
2022-10-19 20:22:58 -0300

    Entrega 2

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
-class Usuario{
-    constructor(nombre,apellido,libros,mascotas){
-        (this.nombre=nombre);
-        (this.apellido=apellido);
-        (this.libros=libros || []);
-        (this.mascotas = mascotas || []);
-    }
+const Contenedor = require("./contenedor");
 
-    getFullName(){
-        return  `${this.nombre} ${this.apellido}`;
-    }
+const productos = new Contenedor("./productos.json");
 
-    addMascota(mascota){
-        return this.mascotas.push(mascota);
-    }
-
-    countMascotas(){
-        return this.mascotas.length;
-    }
-
-    addBook(nombre,autor){
-        this.libros.push({nombre,autor});
-    }
-
-    getBookNames(){
-        return this.libros.map(({ nombre }) => nombre).join(", ");
-    }
-}
-
-let usuario = new Usuario("Nicolas", "Bergami");
-console.log(`El nombre completo del usuario es: ${usuario.getFullName()}.`);
-usuario.addMascota("Gato");
-usuario.addMascota("Perro Bulldog");
-console.log(`Este usuario tiene ${usuario.countMascotas()} mascotas.`);
-usuario.addBook("Don Quijote de la Mancha", "Miguel de Cervantes");
-usuario.addBook("Los cuentos de Canterbury", "Geoffrey Chaucer");
-usuario.addBook("Fisica ", "stewart");
-console.log(`Libros del usuario: ${usuario.getBookNames()}`);
+productos.save({ title: "primero", price: 250 });
