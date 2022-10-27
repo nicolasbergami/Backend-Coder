261e0308aa7824e46ef83c1faa81a152a572a084
nicolasbergami <nicolasbergami2013@gmail.com>
2022-10-26 21:14:08 -0300

    entrega 3

---
+++ Entrega 3/contenedor.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+
+//Servidor************
+const express = require('express');
+const aplicacion = express();
+const port = 8080;
+//****************
+
+class Contenedor {
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+
+  async save(objeto) {
+    const archivo = await fs.promises.readFile(this.nombre, 'utf-8');
+    const archivoParseado = JSON.parse(archivo);
+    let id = 1;
+    archivoParseado.forEach((element, index) => {
+      if (element.id >= id) {
+        id = element.id + 1;
+      }
+    });
+    objeto.id = id;
+    archivoParseado.push(objeto);
+    await fs.promises.writeFile(this.nombre, JSON.stringify(archivoParseado, null, 2));
+    return id;
+  }
+
+  async getById(id) {
+    const archivo = await fs.promises.readFile(this.nombre, 'utf-8');
+    const archivoParseado = JSON.parse(archivo);
+    let objetoSeleccionado = null;
+    archivoParseado.forEach(element => {
+      if (element.id == id) {
+        objetoSeleccionado = element;
+      }
+    });
+    return objetoSeleccionado;
+  }
+
+  async getAll() {
+    const archivo = await fs.promises.readFile(this.nombre, 'utf-8');
+    const archivoParseado = JSON.parse(archivo);
+    return archivoParseado;
+  }
+
+  async deleteById(id) {
+    const archivo = await fs.promises.readFile(this.nombre, 'utf-8');
+    const archivoParseado = JSON.parse(archivo);
+    let indexSeleccionado = -1;
+    archivoParseado.forEach((element, index) => {
+      if (element.id == id) {
+        indexSeleccionado = index;
+      }
+    });
+    if (indexSeleccionado != -1) {
+      archivoParseado.splice(indexSeleccionado, 1);
+      await fs.promises.writeFile(this.nombre, JSON.stringify(archivoParseado, null, 2));
+    }
+    
+  }
+
+  async deleteAll() {
+    const arregloVacio = [];
+    await fs.promises.writeFile(this.nombre, JSON.stringify(arregloVacio, null, 2));
+  }
+}
+
+const implementacion = async () => {
+  // const lista = new Contenedor('productos.txt');
+};
+
+const lista = new Contenedor('productos.txt');
+
+//Endpoints***
+aplicacion.get('/productos', async (peticion, respuesta) => {
+  const all = await lista.getAll();
+  respuesta.json(all);
+});
+
+//devuelve producto random 
+aplicacion.get('/productoRandom', async (peticion, respuesta) => {
+  const random = Math.floor(Math.random() * 4);
+  const all = await lista.getById(random);
+  
+  respuesta.json(all);
+});
+
+aplicacion.get('/indiceRandom', async (peticion, respuesta) => {
+
+  const all = await lista.getAll();
+  const random = Math.floor(Math.random() * all.length);
+  //Llamado a la funcion para obtener el id
+  respuesta.json({
+    random: random
+  });
+});
+
+
+
+
+//Servidor************
+const servidor = aplicacion.listen(port, () => {
+  console.log(`Servidor escuchando: ${servidor.address().port}`);
+});
+
+servidor.on('error', error => console.log(`Error: ${error}`));
+//****************
